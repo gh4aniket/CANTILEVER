a7402d29840db4c1af3dffdb3f6796a481be499c
gh4aniket <116141692+gh4aniket@users.noreply.github.com>
2025-07-17 03:50:11 +0530

    Update index.js

--- Task-Management-Webapp/api/index.js
+++ Task-Management-Webapp/api/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'https://task-management-webapp-task.netlify.app/account'
+    origin: 'https://task-management-webapp-task.netlify.app'
 }))
 
 
